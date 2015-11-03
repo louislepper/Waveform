31e41a5f609d63417943e822d5299e32539eb2a9
Louis Lepper <louis@Louiss-MacBook-Air-2.local>
2015-11-03 22:52:11 +1100

    Fixed tests

--- app/src/androidTest/java/com/louislepper/waveform/ConstantsTest.java
+++ app/src/androidTest/java/com/louislepper/waveform/ConstantsTest.java
@@ -2,17 +2,13 @@ package com.louislepper.waveform;
 
 import junit.framework.TestCase;
 
-import org.junit.Test;
-
-import static junit.framework.Assert.assertTrue;
-
 public class ConstantsTest extends TestCase{
 
-    @Test
     public void testIndexToFrequency(){
         double toneA = Constants.getFrequencyByIndex(0);
         double toneB = Constants.getFrequencyByIndex(1);
-        assertTrue("Tones should be equal.", equals(toneB, Constants.getOffsetNote(toneA, 1)));
+        double offsetNote = Constants.getOffsetNote(toneA, 1);
+        assertTrue("Tones should be equal. Tone A = " + toneA +". Tone B = " + toneB + ". Tone B generated from offset = " + offsetNote, equals(toneB, offsetNote));
     }
 
     private boolean equals(double a, double b) {
